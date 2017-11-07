44f0218e5d1a942f74eb5ee7d7d4cca3fd01c7aa
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-11-07 11:19:59 -0800

    Bitcoin testnet testing

--- wallet/wallet.test.js
+++ wallet/wallet.test.js
@@ -76,13 +76,14 @@ test('wallet refresh balance callback', (done) => {
 
 test('wallet refresh promise', () => {
   jest.setTimeout(60 * 1000)
-  expect.assertions(2)
+  expect.assertions(4)
 
   let wal = new Wallet('75c1209-dbcac5a6-e040977-64a52ae', 'PublicDevAccount')
 
   return wal.load().then(() => {
     return wal.refresh().then(() => {
       expect(wal.keys[0].getBalanceSat('florincoin')).toBe(899600000)
+      expect(wal.keys[0].getBalanceSat('bitcoin_testnet')).toBe(53926225)
       expect(wal.keys[0].getUTXO('florincoin')).toEqual([{
         'address': 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC',
         'amount': 8.996,
@@ -93,6 +94,18 @@ test('wallet refresh promise', () => {
         'txid': 'cdf9581d413286dc1c6ccb7d2b0ca105879c29932364668d579ed2621dffe60a',
         'vout': 1
       }])
+      expect(wal.keys[0].getUTXO('bitcoin_testnet')).toEqual([
+        {
+          "address": "mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL",
+          "txid": "d1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70",
+          "vout": 1,
+          "scriptPubKey": "76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac",
+          "amount": 0.53926225,
+          "satoshis": 53926225,
+          "height": 1225853,
+          "confirmations": 1
+        }
+      ])
     })
   })
 })
@@ -167,6 +180,22 @@ test.skip('wallet payto', () => {
     return wal.refresh().then(() => {
       return wal.keys[0].payTo('florincoin', 'FD42dYEYLfsdr88ukVZ9Pf3rDYs75McM7s', 1, 'yolo swag').then((res) => {
         expect(res.data).toBe('cdf9581d413286dc1c6ccb7d2b0ca105879c29932364668d579ed2621dffe60a')
+      })
+    })
+  })
+})
+
+test.skip('wallet payto testnet', () => {
+  // Skipped test as it won't work without mocking utxo's and balances
+  jest.setTimeout(60 * 1000)
+  expect.hasAssertions()
+
+  let wal = new Wallet('75c1209-dbcac5a6-e040977-64a52ae', 'PublicDevAccount')
+
+  return wal.load().then(() => {
+    return wal.refresh().then(() => {
+      return wal.keys[0].payTo('bitcoin_testnet', 'mnjsTntSJP6E14jVcNsNFBje3tSo7y4pGi', 0.5).then((res) => {
+        expect(res.data).toBe('d1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70')
       })
     })
   })
