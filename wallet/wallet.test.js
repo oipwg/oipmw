154c38c9846f70db11247c411ebd407c4d596bc4
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-11-08 15:10:13 -0800

    bitcoin_testnet test

--- wallet/wallet.test.js
+++ wallet/wallet.test.js
@@ -1,9 +1,5 @@
 const CryptoJS = require('crypto-js')
-const bcrypto = require('bitcoinjs-lib').crypto
-
 const Wallet = require('./wallet')
-const Insight = require('../coins/explorers/insight')
-//jest.mock('../coins/explorers/insight') -- Mock for Wallet.payTo test
 
 test('wallet constructor', () => {
   let wal = new Wallet('75c1209-dbcac5a6-e040977-64a52ae', 'PublicDevAccount')
@@ -83,7 +79,7 @@ test('wallet refresh promise', () => {
   return wal.load().then(() => {
     return wal.refresh().then(() => {
       expect(wal.keys[0].getBalanceSat('florincoin')).toBe(899600000)
-      expect(wal.keys[0].getBalanceSat('bitcoin_testnet')).toBe(53926225)
+      expect(wal.keys[0].getBalanceSat('bitcoin_testnet')).toBe(323926225)
       expect(wal.keys[0].getUTXO('florincoin')).toEqual([{
         'address': 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC',
         'amount': 8.996,
@@ -96,14 +92,34 @@ test('wallet refresh promise', () => {
       }])
       expect(wal.keys[0].getUTXO('bitcoin_testnet')).toEqual([
         {
-          "address": "mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL",
-          "txid": "d1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70",
-          "vout": 1,
-          "scriptPubKey": "76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac",
-          "amount": 0.53926225,
-          "satoshis": 53926225,
-          "height": 1225853,
-          "confirmations": 1
+          'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+          'txid': 'a6cfa5e6c401ae4daad973c3cbd4abf88e07cf36a581480acd4f5ee1ea659ad3',
+          'vout': 0,
+          'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+          'amount': 0.9,
+          'satoshis': 90000000,
+          'height': 1225858,
+          'confirmations': 16
+        },
+        {
+          'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+          'txid': '5d4522368860b94f9ce4ba28633a879a8804e63214d1b4c9bc475856acf6521f',
+          'vout': 0,
+          'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+          'amount': 1.8,
+          'satoshis': 180000000,
+          'height': 1225858,
+          'confirmations': 16
+        },
+        {
+          'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+          'txid': 'd1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70',
+          'vout': 1,
+          'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+          'amount': 0.53926225,
+          'satoshis': 53926225,
+          'height': 1225853,
+          'confirmations': 21
         }
       ])
     })
@@ -130,73 +146,6 @@ test('wallet refresh callback', (done) => {
         'vout': 1
       }])
       done()
-    })
-  })
-})
-
-test.skip('wallet payto', () => {
-  // Requires Insight mocking, which breaks other tests
-  Insight.prototype.getUnspent.mockImplementation((addr) => {
-    switch (addr) {
-      case 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC':
-        return Promise.resolve({
-          data: [{
-            'address': 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC',
-            'amount': 9.997,
-            'confirmations': 6,
-            'confirmationsFromCache': true,
-            'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
-            'ts': 1508836658,
-            'txid': '02c85b5cacecfeede339bb7874f250c7731d986c727283ee0c91cf5c96e52b11',
-            'vout': 1
-          }]
-        })
-      case 'FD42dYEYLfsdr88ukVZ9Pf3rDYs75McM7s':
-        return Promise.resolve({data: []})
-    }
-    return Promise.resolve({data: []})
-  })
-
-  Insight.prototype.getBalance.mockImplementation((addr) => {
-    switch (addr) {
-      case 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC':
-        return Promise.resolve({data: {balanceSat: 999700000}})
-      case 'FD42dYEYLfsdr88ukVZ9Pf3rDYs75McM7s':
-        return Promise.resolve({data: {balanceSat: 0}})
-    }
-    return Promise.resolve({data: {balanceSat: 1}})
-  })
-
-  Insight.prototype.pushTX.mockImplementation((tx) => {
-    return Promise.resolve({data: bcrypto.hash256(Buffer.from(tx, 'hex')).reverse().toString('hex')})
-  })
-
-  jest.setTimeout(60 * 1000)
-  expect.hasAssertions()
-
-  let wal = new Wallet('75c1209-dbcac5a6-e040977-64a52ae', 'PublicDevAccount')
-
-  return wal.load().then(() => {
-    return wal.refresh().then(() => {
-      return wal.keys[0].payTo('florincoin', 'FD42dYEYLfsdr88ukVZ9Pf3rDYs75McM7s', 1, 'yolo swag').then((res) => {
-        expect(res.data).toBe('cdf9581d413286dc1c6ccb7d2b0ca105879c29932364668d579ed2621dffe60a')
-      })
-    })
-  })
-})
-
-test.skip('wallet payto testnet', () => {
-  // Skipped test as it won't work without mocking utxo's and balances
-  jest.setTimeout(60 * 1000)
-  expect.hasAssertions()
-
-  let wal = new Wallet('75c1209-dbcac5a6-e040977-64a52ae', 'PublicDevAccount')
-
-  return wal.load().then(() => {
-    return wal.refresh().then(() => {
-      return wal.keys[0].payTo('bitcoin_testnet', 'mnjsTntSJP6E14jVcNsNFBje3tSo7y4pGi', 0.5).then((res) => {
-        expect(res.data).toBe('d1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70')
-      })
     })
   })
 })
