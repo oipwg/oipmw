9acae70d8c22ad405967ff64cae736e0dee5641f
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-11-08 15:19:01 -0800

    Add Key tests

---
+++ wallet/key.test.js
@@ -0,0 +1,76 @@
+const bcrypto = require('bitcoinjs-lib').crypto
+const Insight = require('../coins/explorers/insight')
+const Key = require('./key')
+
+jest.mock('../coins/explorers/insight')
+Insight.prototype.pushTX.mockImplementation((tx) => {
+  return Promise.resolve(bcrypto.hash256(Buffer.from(tx, 'hex')).reverse().toString('hex'))
+})
+
+test('payto florincoin', () => {
+  let key = new Key('RAo87DBFGGcpLD9KvQrgkVtynPCniTNMW9i8pyVmcY8Hsyzyxy5B', 'florincoin')
+
+  key.coins['florincoin'].utxo = [{
+    'address': 'FDEAciuFexEHy1kiLKRt34e2PybTyhdGZC',
+    'amount': 9.997,
+    'confirmations': 6,
+    'confirmationsFromCache': true,
+    'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+    'ts': 1508836658,
+    'txid': '02c85b5cacecfeede339bb7874f250c7731d986c727283ee0c91cf5c96e52b11',
+    'vout': 1
+  }]
+
+  key.coins['florincoin'].balanceSat = 999700000
+
+  expect.hasAssertions()
+
+  return key.payTo('florincoin', 'FD42dYEYLfsdr88ukVZ9Pf3rDYs75McM7s', 1, 'yolo swag').then((res) => {
+    expect(res).toBe('cdf9581d413286dc1c6ccb7d2b0ca105879c29932364668d579ed2621dffe60a')
+  })
+})
+
+test('key payto testnet', () => {
+  expect.hasAssertions()
+
+  let key = new Key('RAo87DBFGGcpLD9KvQrgkVtynPCniTNMW9i8pyVmcY8Hsyzyxy5B', 'bitcoin_testnet')
+
+  key.coins['bitcoin_testnet'].utxo = [
+    {
+      'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+      'txid': 'a6cfa5e6c401ae4daad973c3cbd4abf88e07cf36a581480acd4f5ee1ea659ad3',
+      'vout': 0,
+      'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+      'amount': 0.9,
+      'satoshis': 90000000,
+      'height': 1225858,
+      'confirmations': 21
+    },
+    {
+      'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+      'txid': '5d4522368860b94f9ce4ba28633a879a8804e63214d1b4c9bc475856acf6521f',
+      'vout': 0,
+      'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+      'amount': 1.8,
+      'satoshis': 180000000,
+      'height': 1225858,
+      'confirmations': 21
+    },
+    {
+      'address': 'mnv1SyZ9cfSt7xMJCCk6tbKpEKBA5gBknL',
+      'txid': 'd1699bd8dd12b2a5e96d922ee3845dd00e0e8a122ff9d5e94f16fd0d411c2e70',
+      'vout': 1,
+      'scriptPubKey': '76a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac',
+      'amount': 0.53926225,
+      'satoshis': 53926225,
+      'height': 1225853,
+      'confirmations': 26
+    }
+  ]
+
+  key.coins['bitcoin_testnet'].balanceSat = 323926225
+
+  return key.payTo('bitcoin_testnet', 'mnjsTntSJP6E14jVcNsNFBje3tSo7y4pGi', 0.5).then((res) => {
+    expect(res).toBe('aac726419638daeb474d4de0cf41199ba7c1687493f865e0a52174a210df4178')
+  })
+})
