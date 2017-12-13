8099843c4f8bc375c69d19d76b18648c3b36cb07
OstlerDev <OstlerDev@users.noreply.github.com>
2017-12-12 16:37:03 -0800

    Add util to exports

--- oipmw.js
+++ oipmw.js
@@ -1,9 +1,11 @@
 import { Wallet } from './wallet/wallet.js'
 import * as Networks from './coins/networks'
+import * as util from './util'
 
 const OIPMW = {
   Wallet: Wallet,
-  Networks: Networks
+  Networks: Networks,
+  util: util
 }
 
 export default OIPMW
