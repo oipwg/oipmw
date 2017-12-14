f3d5a1407b2a6aa92e393b079cd17fd233d3e2c5
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-12-14 15:53:42 -0800

    don't swallow 400 errors

--- util/simplePOST.js
+++ util/simplePOST.js
@@ -2,9 +2,6 @@ const axios = require('axios')
 const callbackify = require('callbackify')
 
 let ax = axios.create()
-ax.defaults.validateStatus = function (status) {
-  return (status >= 200 && status < 300) || status === 400
-}
 
 function simplePOST (url, data) {
   return ax.post(url, data).then((res) => Promise.resolve(res.data))
