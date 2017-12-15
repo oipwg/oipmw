fa8d985f22335214cc805269e5dc9e8b0ba91528
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-12-14 17:06:49 -0800

    fix forgotten test

--- util/simplePOST.test.js
+++ util/simplePOST.test.js
@@ -5,23 +5,8 @@ test('simplePOST promise 400', () => {
 
   return simplePOST('https://florinsight.alexandria.io/api/tx/send',
     {rawtx: '01000000019911327cd1ec6177ea865d2772697652f2dd82d451335c9713aea4ef95e18af9000000006b48304502210094d190f7471d6896d8c54e623576b83ad4324d55ef84bac3d621ae166c86c48202205697ef9cc7846635d6acdc0ae0fe1d1c716771688ef38bd9ddbda0e70c726d2f012103d0184409d51a2fcf0e962a30bff6f95afaab468e607ce61ad3fe32af1f404e0affffffff0200ca9a3b000000001976a9144f3a411d38966b259484338306c46924e616b53388ac2036963b000000001976a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac00000000'})
-    .then((res) => {
-      expect(res).toBeDefined()
-      expect(res).toBe('Transaction rejected by network (code -26). Reason: 18: bad-txns-inputs-spent')
+    .catch((err) => {
+      expect(err.response.status).toBe(400)
+      expect(err.response.data).toBe('Transaction rejected by network (code -26). Reason: 18: bad-txns-inputs-spent')
     })
-})
-
-test('simplePOST callback 400', (done) => {
-  expect.hasAssertions()
-
-  let cb = (err, res) => {
-    expect(err).toBe(null)
-    expect(res).toBeDefined()
-    expect(res).toBe('Transaction rejected by network (code -26). Reason: 18: bad-txns-inputs-spent')
-    done()
-  }
-
-  simplePOST('https://florinsight.alexandria.io/api/tx/send',
-    {rawtx: '01000000019911327cd1ec6177ea865d2772697652f2dd82d451335c9713aea4ef95e18af9000000006b48304502210094d190f7471d6896d8c54e623576b83ad4324d55ef84bac3d621ae166c86c48202205697ef9cc7846635d6acdc0ae0fe1d1c716771688ef38bd9ddbda0e70c726d2f012103d0184409d51a2fcf0e962a30bff6f95afaab468e607ce61ad3fe32af1f404e0affffffff0200ca9a3b000000001976a9144f3a411d38966b259484338306c46924e616b53388ac2036963b000000001976a9145125148dc7f01494071bfe3bf3afc7c612e0bd5388ac00000000'},
-    cb)
 })
